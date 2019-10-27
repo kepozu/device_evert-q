357deca725ab9d36989ff5c54def54982d2f3be1
seba3567 <sebastian26481@gmail.com>
2019-10-27 17:45:31 -0300

    add surface_flinger.use_color

--- sdm660-common/vendor_prop.mk
+++ sdm660-common/vendor_prop.mk
@@ -210,5 +210,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.protected_contents=true \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
+    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
+    ro.surface_flinger.use_color_management=true
 
