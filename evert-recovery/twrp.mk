0ed1ca26b90d7fec8c015379c133f2f8884906bf
seba3567 <sebastian26481@gmail.com>
2019-11-18 15:58:12 -0300

    add twrp ramdisk

---
+++ evert-recovery/twrp.mk
@@ -0,0 +1,7 @@
+# Initial kernel include for avb devices (evert) powered by seba_3567
+
+ 
+TWRP_PATH := device/motorola/evert-recovery
+
+TARGET_PREBUILT_RECOVERY_RAMDISK_IMG := $(TWRP_PATH)/ramdisk-recovery.img
+TARGET_PREBUILT_RECOVERY_RAMDISK := $(TWRP_PATH)/ramdisk-recovery.zip
