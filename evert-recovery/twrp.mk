ce21a763b4c1ae474d36ad3eec88ffb030450347
seba3567 <sebastian26481@gmail.com>
2019-11-20 21:51:01 -0300

    no use ramdisk kernel img

--- evert-recovery/twrp.mk
+++ evert-recovery/twrp.mk
@@ -3,5 +3,4 @@
  
 TWRP_PATH := device/motorola/evert-recovery
 
-TARGET_PREBUILT_RECOVERY_RAMDISK_IMG := $(TWRP_PATH)/ramdisk-recovery.img
 TARGET_PREBUILT_RECOVERY_RAMDISK := $(TWRP_PATH)/ramdisk-recovery.zip
